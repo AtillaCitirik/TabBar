baac7cc408cc5d2f8135a1feb1b5ce279e0d7eea
atilla.citirik <atillactrk@gmail.com>
2024-05-08 00:59:42 +0300

    TabBar oluşturma ve ekranları tabbar'a bağlama

---
+++ TabBarKulanimi/AyarlarVC.swift
@@ -0,0 +1,19 @@
+//
+//  AyarlarVC.swift
+//  TabBarKulanimi
+//
+//  Created by Atilla Çıtırık on 8.05.2024.
+//
+
+import UIKit
+
+class AyarlarVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+}
