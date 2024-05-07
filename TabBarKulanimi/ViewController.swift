4ab66365c6782b14c71796659bd3ee4cfb3a8236
atilla.citirik <atillactrk@gmail.com>
2024-05-08 01:15:23 +0300

    Kod ile TabBar özelleştirme

--- TabBarKulanimi/ViewController.swift
+++ TabBarKulanimi/ViewController.swift
@@ -11,7 +11,34 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        if let tabItems = tabBarController?.tabBar.items {
+            let anasayfaItem = tabItems[0]
+            let ayarlarItem = tabItems[1]
+            anasayfaItem.badgeValue = "3"
+            ayarlarItem.badgeValue = "Yeni"
+        }
+        
+        let appearance = UITabBarAppearance()
+        appearance.backgroundColor = UIColor.systemIndigo
+        
+        renkDegistir(itemappearance: appearance.stackedLayoutAppearance)
+        renkDegistir(itemappearance: appearance.inlineLayoutAppearance)
+        renkDegistir(itemappearance: appearance.compactInlineLayoutAppearance)
+        
+        tabBarController?.tabBar.standardAppearance = appearance
+        tabBarController?.tabBar.scrollEdgeAppearance = appearance
+        
+    }
+    
+    func renkDegistir(itemappearance: UITabBarItemAppearance){
+        itemappearance.selected.iconColor = UIColor.systemOrange
+        itemappearance.selected.titleTextAttributes = [.foregroundColor:UIColor.systemOrange]
+        itemappearance.selected.badgeBackgroundColor = UIColor.systemMint
+        
+        itemappearance.normal.iconColor = UIColor.white
+        itemappearance.normal.titleTextAttributes = [.foregroundColor: UIColor.white]
+        itemappearance.normal.badgeBackgroundColor = UIColor.lightGray
     }
 
 
